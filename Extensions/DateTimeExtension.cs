bd60a699d954e077a70205ecb0a538f07c8e6648
GnuDx2k4 <l7ungdzk4@gmail.com>
2024-07-16 23:41:18 +0700

    Add ToUnixTimeMilliseconds

--- Extensions/DateTimeExtension.cs
+++ Extensions/DateTimeExtension.cs
@@ -3,5 +3,7 @@
     public static class DateTimeExtension
     {
         public static DateTime ConvertFromMilliseconds(this long milliseconds) => DateTimeOffset.FromUnixTimeMilliseconds(milliseconds).LocalDateTime;
+
+        public static long ToUnixTimeMilliseconds(this DateTime dateTime) => new DateTimeOffset(dateTime).ToUnixTimeMilliseconds();
     }
 }
